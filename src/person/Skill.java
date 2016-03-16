ec5f1ec27f73bedd7420ba8ed19c49b298c9e6a4
Slezak Attila <attila.slezak@gmail.com>
2016-03-16 13:21:24 +0100

    Person java files added

---
+++ src/person/Skill.java
@@ -0,0 +1,26 @@
+package person;
+
+public class Skill {
+	
+	private String name;
+	private String description;
+	private double rate;
+	
+	public Skill(String name, String description, double rate) {
+		this.name = name;
+		this.description = description;
+		this.rate = rate;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public double getRate() {
+		return rate;
+	}	
+}
